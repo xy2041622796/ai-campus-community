e62c39a45fb446aea1912af8dbbd22936ca52708
xiaoyang <2041622796@qq.com>
2026-06-28 15:22:16 +0800

    Remove useless card-more button from PostCard

--- src/components/common/PostCard.vue
+++ src/components/common/PostCard.vue
@@ -13,11 +13,6 @@
           </div>
         </div>
       </div>
-      <button class="card-more" @click.stop>
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/>
-        </svg>
-      </button>
     </div>
 
     <!-- 正文 -->
