b762a6f5a90d29335eafd9279023df41e2b8a263
xiaoyang <2041622796@qq.com>
2026-06-28 20:27:19 +0800

    Fix duplicate form-actions div in CreatePostPage

--- src/views/posts/CreatePostPage.vue
+++ src/views/posts/CreatePostPage.vue
@@ -28,7 +28,7 @@
         </el-form-item>
 
         <el-form-item>
-          <div class="form-actions"><div class="form-actions">
+          <div class="form-actions">
             <el-button @click="router.back()">取消</el-button>
             <el-button type="primary" :loading="submitting" @click="handleSubmit">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
