9204c5825d9a121bb7d4a9c91cf2056c7b45be4c
xiaoyang <2041622796@qq.com>
2026-07-01 15:27:36 +0800

    fix: 通知无消息时不再显示红点
    - 移除 v-else 的 badge-dot，只在有未读消息时才显示角标

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -63,7 +63,7 @@
             <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/>
           </svg>
           <span v-if="notifStore.unreadCount > 0" class="badge-num">{{ notifStore.unreadCount > 99 ? "99+" : notifStore.unreadCount }}</span>
-          <span v-else class="badge-dot"></span>
+
         </router-link>
 
         <!-- 用户 -->
